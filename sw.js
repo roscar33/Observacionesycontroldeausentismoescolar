46210fbd3f8e1801d9e4547c3975bb5198474462
roscar33 <140255743+roscar33@users.noreply.github.com>
2025-12-10 17:40:34 -0500

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,14 +1,20 @@
+// Service Worker sencillo para que la PWA funcione bien
+const CACHE_NAME = "acudiente-v1";
+
+// Se instala el SW
 self.addEventListener("install", (event) => {
   self.skipWaiting();
 });
 
+// Se activa el SW
 self.addEventListener("activate", (event) => {
-  return self.clients.claim();
+  event.waitUntil(self.clients.claim());
 });
 
+// Intercepta las peticiones para poder usar caché
 self.addEventListener("fetch", (event) => {
   event.respondWith(
-    caches.open("v1").then((cache) => {
+    caches.open(CACHE_NAME).then((cache) => {
       return fetch(event.request)
         .then((response) => {
           cache.put(event.request, response.clone());
